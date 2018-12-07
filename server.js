ea4cac431b040e117396f37e6d5d39162749084d
Mark Deazley <mdeazley@gmail.com>
2018-12-07 03:22:16 -0700

    ES6 mods.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const queueName = 'hello';
 const rabbitmq_host = 'ap1000';
 
 const amqp = require('amqplib');
@@ -6,12 +7,12 @@ const uuidv4 = require('uuid/v4');
 
 const { Observable, timer } = require('rxjs');
 
-fs.readFile('tls/userpass', 'utf8', function(err, contents) {
+fs.readFile('tls/userpass', 'utf8', (err, contents) => {
     if (err) {
       console.error("Unable to read \'password\' file! ... " + err);
       process.exit();
     }
-    var userpass = contents.replace(/^\s+|\s+$/g, '');
+    const userpass = contents.replace(/^\s+|\s+$/g, '');
     const sessionId = uuidv4();
     send(rabbitmq_host, userpass, sessionId);
 });
@@ -20,30 +21,29 @@ fs.readFile('tls/userpass', 'utf8', function(err, contents) {
 function send(host,userpass, sessionId) {
   const auth = encodeURIComponent(userpass);
   const url = 'amqps://' + auth + '@' + host;
-  var opts = {
+  const opts = {
     cert: fs.readFileSync('tls/client_certificate.pem'),
     key: fs.readFileSync('tls/client_key.pem'),
     ca: [fs.readFileSync('tls/ca_certificate.pem')]
   };
-  amqp.connect(url, opts).then(function(conn) {
-    return conn.createChannel().then(function(ch) {
-      var q = 'hello';
-      var qp = ch.assertQueue(q, {durable: false});
-      return qp.then(function(qstate) {
+  amqp.connect(url, opts).then((conn) => {
+    return conn.createChannel().then((ch) => {
+      const qp = ch.assertQueue(queueName, {durable: false});
+      return qp.then((qstate) => {
         console.log(qstate);
-        const subscription = timer(0,1000).subscribe((msgnum) => {
+        const timerSubscription = timer(0,1000).subscribe((msgnum) => {
           const msg = {
              'id': sessionId,
              'mn': msgnum,
              'ts': Date.now() / 1000
           };
           const msgJson = JSON.stringify(msg);
-          ch.sendToQueue(q, Buffer.from(msgJson));
+          ch.sendToQueue(queueName, Buffer.from(msgJson));
           console.log(msgJson);
         });
         process.on('SIGINT', () => {
           console.log('\nSIGINT!');
-          subscription.unsubscribe();
+          timerSubscription.unsubscribe();
           ch.close();
           conn.close();
         });
