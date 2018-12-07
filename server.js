8af1a7184ade9a51b58dbc5e91ab096ce4e13a0a
Mark Deazley <mdeazley@gmail.com>
2018-12-07 09:56:57 -0700

    Implemented imuMessageService.

--- server.js
+++ server.js
@@ -1,22 +1,40 @@
 const queueName = 'hello';
 const rabbitmq_host = 'ap1000';
+const UPDATE_RATE_MSEC = process.argv[2] || 100;
+
+const PROGRAM_NAME = process.argv[1].replace(/^\/.*\//,'');
+
+const fs = require('fs');
+const os = require('os');
 
 const amqp = require('amqplib');
-const fs = require('fs');
 const uuidv4 = require('uuid/v4');
-
 const { Observable, timer } = require('rxjs');
+const imu = require("node-sense-hat").Imu;
 
-fs.readFile('tls/userpass', 'utf8', (err, contents) => {
-    if (err) {
-      console.error("Unable to read \'password\' file! ... " + err);
-      process.exit();
-    }
-    const userpass = contents.replace(/^\s+|\s+$/g, '');
-    const sessionId = uuidv4();
-    send(rabbitmq_host, userpass, sessionId);
-});
+const IMU = new imu.IMU();
 
+function imuMessageService(sessionId, period, subCallback) {
+  return Observable.create((observer) => {
+    console.log("TIMER PERIOD: %d msec", period);
+    const timerSubscription = timer(0,period).subscribe((msgnum) => {
+      IMU.getValue((err, data) => {
+        if (err !== null) {
+          console.error("Could not read sensor data: ", err);
+          return;
+        }
+        data.mn = msgnum;
+        data.id = sessionId;
+        data.host = os.hostname();
+
+        delete data.humidity;
+        delete data.temperature;
+        observer.next(data);
+      }); 
+    });
+    subCallback(timerSubscription);
+  });
+}
 
 function send(host,userpass, sessionId) {
   const auth = encodeURIComponent(userpass);
@@ -31,19 +49,18 @@ function send(host,userpass, sessionId) {
       const qp = ch.assertQueue(queueName, {durable: false});
       return qp.then((qstate) => {
         console.log(qstate);
-        const timerSubscription = timer(0,1000).subscribe((msgnum) => {
-          const msg = {
-             'id': sessionId,
-             'mn': msgnum,
-             'ts': Date.now() / 1000
-          };
-          const msgJson = JSON.stringify(msg);
+        var timerSubscription;
+        imuMessageService(sessionId, UPDATE_RATE_MSEC, (ts) => { timerSubscription = ts; })
+        .subscribe(data => {
+          const msgJson = JSON.stringify(data);
           ch.sendToQueue(queueName, Buffer.from(msgJson));
           console.log(msgJson);
         });
         process.on('SIGINT', () => {
-          console.log('\nSIGINT!');
-          timerSubscription.unsubscribe();
+          console.log('\n' + PROGRAM_NAME + ' Exiting!');
+          if (timerSubscription) { 
+            timerSubscription.unsubscribe();
+          }
           ch.close();
           conn.close();
         });
@@ -51,3 +68,18 @@ function send(host,userpass, sessionId) {
     });
   }).catch(console.warn);
 }
+
+function main() {
+  fs.readFile('tls/userpass', 'utf8', (err, contents) => {
+    if (err) {
+      console.error("Unable to read \'tls/userpass\' file! ... " + err);
+      process.exit();
+    }
+    const userpass = contents.replace(/^\s+|\s+$/g, '');
+    const sessionId = uuidv4();
+    send(rabbitmq_host, userpass, sessionId);
+  });
+}
+
+console.log(JSON.stringify(process.env,null,2));
+main();
